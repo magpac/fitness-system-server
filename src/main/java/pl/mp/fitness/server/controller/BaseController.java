cbcb2f2135b8ac3d8171137ceed403d35b6c5e80
Magdalena Pączek <magdalena.paczek1@gmail.com>
2023-01-11 10:56:24 +0100

    chore: project initialization

---
+++ src/main/java/pl/mp/fitness/server/controller/BaseController.java
@@ -0,0 +1,13 @@
+package pl.mp.fitness.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
